d59695b4884882e26f1eee30bfa0bf6e187adee8
arunabha369 <119969705+arunabha369@users.noreply.github.com>
2025-05-21 16:56:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,113 @@
 import streamlit as st
+from streamlit.runtime.caching import cache_data
 import pickle
 import pandas as pd
 import requests
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import ast
 
-load_dotenv()
-API_KEY = os.getenv("TMDB_API_KEY")
+# load_dotenv()
+# API_KEY = os.getenv("TMDB_API_KEY")
 
-movies_data = pd.read_csv('tmdb_5000_movies.csv')
-
-def extract_genres(genre_str):
-    genres = ast.literal_eval(genre_str)
-    return [genre['name'] for genre in genres]
-
-movies_data['genres_list'] = movies_data['genres'].apply(extract_genres)
-
-all_genres = sorted(list({genre for sublist in movies_data['genres_list'] for genre in sublist}))
+API_KEY = st.secrets["TMDB_API_KEY"]
 
 
+# movies_data = pd.read_csv('tmdb_5000_movies.csv')
+# credits_data = pd.read_csv("tmdb_5000_credits.csv")
+
+@st.cache_data
+def load_movie_data():
+    return pd.read_csv('tmdb_5000_movies.csv')
+
+@st.cache_data
+def load_credits_data():
+    return pd.read_csv('tmdb_5000_credits.csv')
+
+movies_data = load_movie_data()
+credits_data = load_credits_data()
+
+
+# def extract_genres(genre_str):
+#     genres = ast.literal_eval(genre_str)
+#     return [genre['name'] for genre in genres]
+
+# movies_data['genres_list'] = movies_data['genres'].apply(extract_genres)
+
+# all_genres = sorted(list({genre for sublist in movies_data['genres_list'] for genre in sublist}))
+
+@st.cache_data
+def extract_and_list_genres(df):
+    def extract_genres(genre_str):
+        genres = ast.literal_eval(genre_str)
+        return [genre['name'] for genre in genres]
+
+    df['genres_list'] = df['genres'].apply(extract_genres)
+    all_genres = sorted(list({genre for sublist in df['genres_list'] for genre in sublist}))
+    return df, all_genres
+
+movies_data, all_genres = extract_and_list_genres(movies_data)
+
+
+
+
+# def extract_cast(cast_str):
+#     try:
+#         cast_list = ast.literal_eval(cast_str)
+#         return [person['name'] for person in cast_list]
+#     except:
+#         return []
+
+# credits_data['cast_names'] = credits_data['cast'].apply(extract_cast)
+
+# all_actors = sorted(list({actor for sublist in credits_data['cast_names'] for actor in sublist}))
+
+
+# def extract_director(crew_str):
+#     try:
+#         crew_list = ast.literal_eval(crew_str)
+#         for person in crew_list:
+#             if person['job'] == 'Director':
+#                 return person['name']
+#     except:
+#         return None
+
+# credits_data['director'] = credits_data['crew'].apply(extract_director)
+
+# all_directors = sorted(credits_data['director'].dropna().unique())
+
+
+@st.cache_data
+def enrich_credits(df):
+    def extract_cast(cast_str):
+        try:
+            cast_list = ast.literal_eval(cast_str)
+            return [person['name'] for person in cast_list]
+        except:
+            return []
+
+    def extract_director(crew_str):
+        try:
+            crew_list = ast.literal_eval(crew_str)
+            for person in crew_list:
+                if person['job'] == 'Director':
+                    return person['name']
+        except:
+            return None
+
+    df['cast_names'] = df['cast'].apply(extract_cast)
+    df['director'] = df['crew'].apply(extract_director)
+    return df
+
+credits_data = enrich_credits(credits_data)
+
+all_actors = sorted(list({actor for sublist in credits_data['cast_names'] for actor in sublist}))
+all_directors = sorted(credits_data['director'].dropna().unique())
+
+
+
+
+@st.cache_data(show_spinner=False)
 def fetch_poster(movie_id):
     try:
         url = f"http://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
@@ -63,10 +151,42 @@ def recommend_by_popularity(selected_genre):
     posters = [fetch_poster(mid) for mid in top_movies['id']]
     return titles, posters
 
+def recommend_by_cast(selected_actor):
+    matched = credits_data[credits_data['cast_names'].apply(lambda x: selected_actor in x)]
+    merged = matched.merge(movies_data, on="title")
 
-movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+    top_movies = merged.sort_values(by="vote_average", ascending=False).drop_duplicates(subset='title').head(5)
+
+    titles = top_movies['title'].tolist()
+    posters = [fetch_poster(mid) for mid in top_movies['id']]
+    return titles, posters
+
+def recommend_by_director(selected_director):
+    matched = credits_data[credits_data['director'] == selected_director]
+    merged = matched.merge(movies_data, on="title")
+    
+    top_movies = merged.sort_values(by="vote_average", ascending=False).drop_duplicates(subset='title').head(5)
+
+    titles = top_movies['title'].tolist()
+    posters = [fetch_poster(mid) for mid in top_movies['id']]
+    return titles, posters
+
+
+
+# movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+# movies = pd.DataFrame(movies_dict)
+# similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+@st.cache_data
+def load_pickle_file(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+movies_dict = load_pickle_file('movies_dict.pkl')
+similarity = load_pickle_file('similarity.pkl')
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+
 
 st.title("Movie Recommender System")
 
@@ -125,3 +245,33 @@ if st.button("Recommend by Popularity"):
             st.text(names[i])
             st.image(posters[i])
 
+
+st.write("---")
+st.subheader("Actor-Based Recommendations")
+
+selected_actor = st.selectbox("Select Actor", all_actors)
+
+if st.button("Recommend by Actor"):
+    names, posters = recommend_by_cast(selected_actor)
+    st.markdown(f"#### Top Movies Featuring {selected_actor}")
+    cols = st.columns(5)
+    for i in range(len(names)):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
+
+
+st.write("---")
+st.subheader("Director-Based Recommendations")
+
+selected_director = st.selectbox("Select Director", all_directors)
+
+if st.button("Recommend by Director"):
+    names, posters = recommend_by_director(selected_director)
+    st.markdown(f"#### Top Movies by {selected_director}")
+    cols = st.columns(5)
+    for i in range(len(names)):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
+           
